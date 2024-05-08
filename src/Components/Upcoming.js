4749556b3b251ad7cbd489940b56c66190ffb0ee
imranabdisalan7 <agmraan@gmail.com>
2024-05-08 19:56:19 +0300

    fixed a bug

--- src/Components/Upcoming.js
+++ src/Components/Upcoming.js
@@ -1,138 +1,64 @@
-import React, {createContext, useContext, useReducer} from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useGlobalContext } from '../context/global'
+import styled from 'styled-components'
+import Sidebar from './Sidebar'
 
-const GlobalContext = createContext();
+function Upcoming({rendered}) {
+    const {upcomingAnime ,isSearch, searchResults} = useGlobalContext()
 
-const baseUrl = "https://api.jikan.moe/v4";
-
-//actions
-const LOADING = "LOADING";
-const SEARCH = "SEARCH";
-const GET_POPULAR_ANIME = "GET_POPULAR_ANIME";
-const GET_UPCOMING_ANIME = "GET_UPCOMING_ANIME";
-const GET_AIRING_ANIME = "GET_AIRING_ANIME";
-const GET_PICTURES = "GET_PICTURES";
-
-//reducer
-const reducer = (state, action) => {
-    switch(action.type){
-        case LOADING:
-            return {...state, loading: true}
-        case GET_POPULAR_ANIME:
-            return {...state, popularAnime: action.payload, loading: false}
-        case SEARCH:
-            return {...state, searchResults: action.payload, loading: false}
-        case GET_UPCOMING_ANIME:
-            return {...state, upcomingAnime: action.payload, loading: false}
-        case GET_AIRING_ANIME:
-            return {...state, airingAnime: action.payload, loading: false}
-        case GET_PICTURES:
-            return {...state, pictures: action.payload, loading: false}
-        default:
-            return state;
-    }
-}
-
-
-export const GlobalContextProvider = ({children}) => {
-    //intial state
-    const intialState = {
-        popularAnime: [],
-        upcomingAnime: [],
-        airingAnime: [],
-        pictures: [],
-        isSearch: false,
-        searchResults: [],
-        loading: false,
-    }
-
-    const [state, dispatch] = useReducer(reducer, intialState);
-    const [search, setSearch] = React.useState('');
-
-
-    //handle change
-    const handleChange = (e) => {
-        setSearch(e.target.value);
-        if(e.target.value === ''){
-            state.isSearch = false;
-        }
-    }
-
-
-    //handle submit
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(search){
-            searchAnime(search);
-            state.isSearch = true;
+    const conditionalRender = () => {
+        if(!isSearch && rendered === 'upcoming'){
+            return upcomingAnime?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
         }else{
-            state.isSearch = false;
-            alert('Please enter a search term')
+            return searchResults?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
         }
     }
 
-    //fetch popular anime
-    const getPopularAnime = async () => {
-        dispatch({type: LOADING})
-        const response = await fetch(`${baseUrl}/top/anime?filter=bypopularity`);
-        const data = await response.json();
-        dispatch({type: GET_POPULAR_ANIME, payload: data.data})
-    }
-
-    //fetch upcoming anime
-    const getUpcomingAnime = async () => {
-        dispatch({type: LOADING})
-        const response = await fetch(`${baseUrl}/top/anime?filter=upcoming`);
-        const data = await response.json();
-        dispatch({type: GET_UPCOMING_ANIME, payload: data.data})
-    }
-
-
-    //fetch airing anime
-    const getAiringAnime = async () => {
-        dispatch({type: LOADING})
-        const response = await fetch(`${baseUrl}/top/anime?filter=airing`);
-        const data = await response.json();
-        dispatch({type: GET_AIRING_ANIME, payload: data.data})
-    }
-
-    //search anime
-    const searchAnime = async (anime) => {
-        dispatch({type: LOADING})
-        const response = await fetch(`https://api.jikan.moe/v4/anime?q=${anime}&order_by=popularity&sort=asc&sfw`);
-        const data = await response.json();
-        dispatch({type: SEARCH, payload: data.data})
-    }
-
-    //get anime pictures
-    const getAnimePictures = async (id) => {
-        dispatch({type: LOADING})
-        const response = await fetch(`https://api.jikan.moe/v4/characters/${id}/pictures`);
-        const data = await response.json();
-        dispatch({type: GET_PICTURES, payload: data.data})
-    }
-
-    //initial render
-    React.useEffect(() => {
-        getPopularAnime();
-    }, [])
-
-    return(
-        <GlobalContext.Provider value={{
-            ...state,
-            handleChange,
-            handleSubmit,
-            searchAnime,
-            search,
-            getPopularAnime,
-            getUpcomingAnime,
-            getAiringAnime,
-            getAnimePictures 
-        }}>
-            {children}
-        </GlobalContext.Provider>
+    return (
+        <PopularStyled>
+            <div className="upcoming-anime">
+                {conditionalRender()}
+            </div>
+            <Sidebar />
+        </PopularStyled>
     )
 }
 
-export const useGlobalContext = () => {
-    return useContext(GlobalContext);
-}
+const PopularStyled = styled.div`
+    display: flex;
+    .upcoming-anime{
+        margin-top: 2rem;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+        padding-left: 5rem;
+        padding-right: 0;
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        grid-gap: 2rem;
+        background-color: #fff;
+        border-top: 5px solid #e5e7eb;
+        a{
+            height: 500px;
+            border-radius: 7px;
+            border: 5px solid #e5e7eb;
+        }
+        a img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 5px;
+        }
+    }
+`;
+
+export default Upcoming
